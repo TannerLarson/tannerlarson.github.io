b8dad40a04318d3a4796952215946f501f2470c0
Tanner Larson <tannerl@qualtrics.com>
2026-01-18 13:37:12 -0700

    Initial commit

---
+++ js/pages-config.js
@@ -0,0 +1,8 @@
+// Configuration for all subpages
+// To add a new page:
+// 1. Add an entry here
+// 2. Create the corresponding directory and index.html
+const PAGES = [
+    { path: '/page1', title: 'Page 1' },
+    { path: '/page2', title: 'Page 2' }
+];
